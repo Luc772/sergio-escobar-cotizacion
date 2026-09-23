0ce108fd4a548a593367e54a07b59e8a09629994
Luc772 <lahlarron@gmail.com>
2026-09-23 17:10:49 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/* Configuración compartida por index.html (clientes) y admin.html (Sergio) */
-const CONFIG = {
-  NOMBRE: 'Sergio Escobar',
-  WHATSAPP: '',      // Formato internacional sin +, ej: 56912345678
-  EMAIL: '',         // Correo para recibir solicitudes (opción "Enviar por correo")
-  SHEETS_URL: '',    // URL /exec del Google Apps Script (ver README)
-  VALIDEZ_DIAS: 5    // Vigencia de la cotización en el PDF
-};
